664ef48eba0cf86c35dc426529e7734839a68389
iamvigneshwars <iamvigneshwars@gmail.com>
2025-03-08 22:40:16 +0000

    save on auto format

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -118,6 +118,11 @@ return {
                 },
             })
             vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                callback = function()
+                    vim.lsp.buf.format()
+                end,
+            })
         end,
     }
 }
