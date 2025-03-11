2114c8dfa1c06894841baf250a47922b46104bcd
iamvigneshwars <iamvigneshwars@gmail.com>
2025-03-11 22:31:41 +0000

    pitch black background

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -2,15 +2,26 @@ return {
     {
         "EdenEast/nightfox.nvim",
         config = function()
+            require("nightfox").setup({
+                options = {
+                    transparent = false,
+                },
+                groups = {
+                    all = {
+                        -- Set the background to pitch black (#000000)
+                        Normal = { bg = "#000000" },
+                        NormalFloat = { bg = "#000000" },
+
+                        -- These settings help maintain consistency
+                        StatusLine = { bg = "#000000" },
+                        VertSplit = { bg = "#000000" },
+                        TabLine = { bg = "#000000" },
+                        TabLineFill = { bg = "#000000" },
+                    }
+                }
+            })
+
             vim.cmd.colorscheme("carbonfox")
         end
     }
-    -- {
-    -- 	"catppuccin/nvim",
-    -- 	name = "catppuccin",
-    -- 	priority = 1000,
-    -- 	config = function()
-    -- 		vim.cmd.colorscheme("catppuccin")
-    -- 	end,
-    -- },
 }
