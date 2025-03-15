8b4cdcb7e54a9a4a018ede85017a245429859a0d
iamvigneshwars <iamvigneshwars@gmail.com>
2025-03-15 19:56:48 +0000

    auto pair

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -124,5 +124,25 @@ return {
                 end,
             })
         end,
+    },
+    -- Auto-pairs plugin
+    {
+        "windwp/nvim-autopairs",
+        event = "InsertEnter",
+        config = function()
+            require("nvim-autopairs").setup({
+                check_ts = true,
+                ts_config = {
+                    lua = { 'string' },
+                }
+            })
+            local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+            local cmp = require('cmp')
+            cmp.event:on(
+                'confirm_done',
+                cmp_autopairs.on_confirm_done()
+            )
+        end
     }
 }
+
