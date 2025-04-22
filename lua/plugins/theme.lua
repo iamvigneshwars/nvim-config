29d187a11d41113fa50e49df6ee2362eb4bbb4d8
iamvigneshwars <iamvigneshwars@gmail.com>
2025-04-22 17:21:05 -0400

    tokyodark

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -23,21 +23,21 @@
 --     }
 -- }
 
--- return {
---     "tiagovla/tokyodark.nvim",
---     opts = {
---
---     },
---     config = function(_, opts)
---         require("tokyodark").setup(opts)
---         vim.cmd [[colorscheme tokyodark]]
---         vim.api.nvim_set_hl(0, "Normal", { bg = "#000000" })
---         vim.api.nvim_set_hl(0, "NormalNC", { bg = "#000000" })
---         vim.api.nvim_set_hl(0, "SignColumn", { bg = "#000000" })
---         vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "#000000" })
---         vim.api.nvim_set_hl(0, "Directory", { fg = "#5e81ac" })
---     end,
--- }
+return {
+    "tiagovla/tokyodark.nvim",
+    opts = {
+
+    },
+    config = function(_, opts)
+        require("tokyodark").setup(opts)
+        vim.cmd [[colorscheme tokyodark]]
+        vim.api.nvim_set_hl(0, "Normal", { bg = "#000000" })
+        vim.api.nvim_set_hl(0, "NormalNC", { bg = "#000000" })
+        vim.api.nvim_set_hl(0, "SignColumn", { bg = "#000000" })
+        vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "#000000" })
+        vim.api.nvim_set_hl(0, "Directory", { fg = "#5e81ac" })
+    end,
+}
 -- return {
 --     {
 --         "Mofiqul/vscode.nvim",
@@ -83,27 +83,57 @@
 --         end
 --     }
 -- }
-return {
-    {
-        "eldritch-theme/eldritch.nvim",
-        lazy = false,
-        priority = 1000,
-        opts = {
-            transparent = false,
-            colors = {
-                bg = "#000000"
-            },
-        },
-        config = function(_, opts)
-            require("eldritch").setup(opts)
-            vim.cmd("colorscheme eldritch")
-            vim.cmd("hi Normal guibg=#000000 ctermbg=0")
-            vim.cmd("hi NormalFloat guibg=#000000 ctermbg=0")
-            vim.cmd("hi NonText guibg=#000000 ctermbg=0")
-            vim.cmd("hi LineNr guibg=#000000 ctermbg=0")
-            vim.cmd("hi SignColumn guibg=#000000 ctermbg=0")
-            vim.cmd("hi EndOfBuffer guibg=#000000 ctermbg=0")
-            vim.opt.termguicolors = true
-        end,
-    }
-}
+-- return {
+--     {
+--         "eldritch-theme/eldritch.nvim",
+--         lazy = false,
+--         priority = 1000,
+--         opts = {
+--             transparent = false,
+--             colors = {
+--                 bg = "#000000"
+--             },
+--         },
+--         config = function(_, opts)
+--             require("eldritch").setup(opts)
+--             vim.cmd("colorscheme eldritch")
+--             vim.opt.termguicolors = true
+--         end,
+--     }
+-- }
+-- return {
+--     {
+--         "sainnhe/sonokai",
+--         lazy = false,
+--         priority = 1000,
+--         config = function()
+--             vim.g.sonokai_style = "default"
+--             vim.opt.termguicolors = true
+--             vim.g.sonokai_better_performance = 1
+--             vim.g.sonokai_enable_italic = 0
+--             vim.g.sonokai_disable_italic_comment = 1
+--             vim.g.sonokai_transparent_background = 0
+--             vim.g.sonokai_diagnostic_virtual_text = "colored"
+--             vim.cmd("colorscheme sonokai")
+--         end,
+--     }
+-- }
+-- return {
+--     "folke/tokyonight.nvim",
+--     priority = 1000,
+--     config = function()
+--         require("tokyonight").setup({
+--             style = "night", -- The darkest variant
+--             transparent = false,
+--             styles = {
+--                 sidebars = "dark",
+--                 floats = "dark",
+--             },
+--             on_colors = function(colors)
+--                 colors.bg = "#000000"
+--                 colors.bg_dark = "#000000"
+--             end
+--         })
+--         vim.cmd.colorscheme "tokyonight"
+--     end
+-- }
