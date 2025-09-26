493ab8200e6520971ec68930759fdbf3b0a6456b
iamvigneshwars <iamvigneshwars@gmail.com>
2025-09-26 13:14:25 +0100

    update theme to nightfox

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,42 +1,42 @@
--- return {
---     {
---         "EdenEast/nightfox.nvim",
---         config = function()
---             require("nightfox").setup({
---                 options = {
---                     transparent = false,
---                 },
---                 groups = {
---                     all = {
---                         Normal = { bg = "#000000" },
---                         NormalFloat = { bg = "#000000" },
---                         StatusLine = { bg = "#000000" },
---                         VertSplit = { bg = "#000000" },
---                         TabLine = { bg = "#000000" },
---                         TabLineFill = { bg = "#000000" },
---                     }
---                 }
---             })
---
---             vim.cmd.colorscheme("carbonfox")
---         end
---     }
--- }
 return {
-    "tiagovla/tokyodark.nvim",
-    opts = {
+    {
+        "EdenEast/nightfox.nvim",
+        config = function()
+            require("nightfox").setup({
+                options = {
+                    transparent = false,
+                },
+                groups = {
+                    all = {
+                        Normal = { bg = "#000000" },
+                        NormalFloat = { bg = "#000000" },
+                        StatusLine = { bg = "#000000" },
+                        VertSplit = { bg = "#000000" },
+                        TabLine = { bg = "#000000" },
+                        TabLineFill = { bg = "#000000" },
+                    }
+                }
+            })
 
-    },
-    config = function(_, opts)
-        require("tokyodark").setup(opts)
-        vim.cmd [[colorscheme tokyodark]]
-        vim.api.nvim_set_hl(0, "Normal", { bg = "#000000" })
-        vim.api.nvim_set_hl(0, "NormalNC", { bg = "#000000" })
-        vim.api.nvim_set_hl(0, "SignColumn", { bg = "#000000" })
-        vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "#000000" })
-        vim.api.nvim_set_hl(0, "Directory", { fg = "#5e81ac" })
-    end,
+            vim.cmd.colorscheme("carbonfox")
+        end
+    }
 }
+-- return {
+--     "tiagovla/tokyodark.nvim",
+--     opts = {
+--
+--     },
+--     config = function(_, opts)
+--         require("tokyodark").setup(opts)
+--         vim.cmd [[colorscheme tokyodark]]
+--         vim.api.nvim_set_hl(0, "Normal", { bg = "#000000" })
+--         vim.api.nvim_set_hl(0, "NormalNC", { bg = "#000000" })
+--         vim.api.nvim_set_hl(0, "SignColumn", { bg = "#000000" })
+--         vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "#000000" })
+--         vim.api.nvim_set_hl(0, "Directory", { fg = "#5e81ac" })
+--     end,
+-- }
 -- return {
 --     {
 --         "Mofiqul/vscode.nvim",
